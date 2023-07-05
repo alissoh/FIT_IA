864696e7de0503173f6f013a9b4dd4ec8b1656a7
alisson <lukadtavared@gmail.com>
2023-07-05 00:14:07 -0300

    first commit

---
+++ index.mjs
@@ -0,0 +1,60 @@
+import express from "express"
+import bodyParser from "body-parser"
+import 'dotenv/config'
+import { Configuration, OpenAIApi } from "openai"
+const app = express()
+const port = 3000
+
+
+
+const configuration = new Configuration({
+  apiKey:process.env.YOU_WONT_USE_MY_KEY_ILAN,
+});
+const openai = new OpenAIApi(configuration);
+
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs')
+app.set('views','./view')//o ejs encontra o view automatica logo deixa de ser nessesario adicionar essa função, mas caso voce queria definir um view espefico, é com essa função que é possivel.
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.render('center',{textCompletion})
+})
+let textCompletion = '';
+
+app.post("/add", async (req, res) => {
+  const peso = req.body.input1;
+  const idade = req.body.input2;
+  const prompt = `
+
+rotina de alimentação para fins de estudo somente, para perde peço com base sua idade que é essa ${idade} e peso que é esse  ${peso}. faça dessa forma a resposta  faça uma separação de cada um dos momentos de se alimentar
+café da manhã: informe aqui
+almoço: informe aqui
+café da tarde: informe aqui
+almoço: informe aqui
+faça uma recomedação saldavel e um alerta com base a idade e o peso
+
+		`
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    max_tokens: 1000,
+    temperature: 0,
+    messages: [{role: "user", content: prompt}],
+  });
+	 textCompletion = completion.data.choices[0].message.content;
+  res.render('center',{textCompletion})
+  
+  
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${process.env.ola}`)
+})
